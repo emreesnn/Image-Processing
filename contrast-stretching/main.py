87fda5ba57905888139a8bc98deb79cb842f7a66
emreesnn <emreesnn17@gmail.com>
2024-01-05 06:33:27 +0300

    Eksiklerin tamamlanması

--- contrast-stretching/main.py
+++ contrast-stretching/main.py
@@ -3,10 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def contrast_stretching_color(image_path, min_out=0, max_out=255):
-    # Renkli görüntüyü oku
     image = cv2.imread(image_path)
 
-    # Renk kanallarını ayrı ayrı işle
     stretched_channels = []
     for channel in cv2.split(image):
         min_in = np.min(channel)
@@ -15,24 +13,19 @@ def contrast_stretching_color(image_path, min_out=0, max_out=255):
         stretched_channel = np.clip(stretched_channel, min_out, max_out).astype(np.uint8)
         stretched_channels.append(stretched_channel)
 
-    # Renkli görüntüyü birleştir
     stretched_image = cv2.merge(stretched_channels)
 
-    # Görüntülerin yan yana gösterimi için subplot kullanma
     plt.figure(figsize=(12, 6))
 
-    # Orjinal renkli görüntü
     plt.subplot(1, 2, 1)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title('Orjinal Görüntü')
 
-    # Kontrast germe sonrası renkli görüntü
     plt.subplot(1, 2, 2)
     plt.imshow(cv2.cvtColor(stretched_image, cv2.COLOR_BGR2RGB))
     plt.title('Kontrast Germe Sonrası')
 
     plt.show()
 
-# Örnek olarak bir renkli görüntü yolu verelim
 image_path_color = "contrast-stretching-image.png"
 contrast_stretching_color(image_path_color)
--- gaussian/main.py
+++ gaussian/main.py
@@ -3,28 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def gaussian_blur(image_path, kernel_size=3):
-    # Görüntüyü oku
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Gaussian blurlaştırma işlemi
     blurred_image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
 
-    # Görüntülerin yan yana gösterimi için subplot kullanma
     plt.figure(figsize=(12, 6))
 
-    # Orjinal görüntü
     plt.subplot(1, 2, 1)
     plt.imshow(image, cmap='gray')
     plt.title('Orjinal Görüntü')
 
-    # Gaussian blurlaştırma ile elde edilmiş görüntü
     plt.subplot(1, 2, 2)
     plt.imshow(blurred_image, cmap='gray')
     plt.title(f'Gaussian Blurlaştırma (Kernel Boyutu = {kernel_size})')
 
-    # Görüntüleri göster
     plt.show()
 
-# Örnek olarak bir görüntü yolu verelim
 image_path = "image1.png"
-gaussian_blur(image_path, kernel_size=9)  # Kernel boyutunu istediğiniz şekilde ayarlayabilirsiniz
+gaussian_blur(image_path, kernel_size=9)
